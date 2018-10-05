3af76de633ff6d7c3d18ec9c285c9093c8d378e0
Harry de Winton <harrydewinton@gmail.com>
2018-10-05 10:51:23 +0100

    import laser data line from orginal scriptcopied

--- process_laser_data.m
+++ process_laser_data.m
@@ -1,2 +1,5 @@
 %% Processsing laser data
 % this is a trial script to try and incorporate the 
+
+% import laser data
+[t_daq,Diode,~,~,x,y,~] = importfile(laserdatafn,laserdata.startrow,laserdata.endrow);
