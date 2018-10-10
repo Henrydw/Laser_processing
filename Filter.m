f7438fdab87f4012e9c320d26ef063ee1fcb2771
Harry de Winton <harrydewinton@gmail.com>
2018-10-10 17:16:16 +0100

    added import for band pass filter effciency curves

--- Filter.m
+++ Filter.m
@@ -2,10 +2,14 @@
 
 %% imports:
 
-% ren_cal(.csv) data:
+% ren_cal(.csv):
 path_ren_cal = "calibration_data/ren_cal.csv";
 optical_data = csvread(path_ren_cal,2,0);
 
-% load spectral repsonse of camera:
+% spectral repsonse of camera:
 path_spectral = "calibration_data/SA5_Spectral_Response_Curve.csv";
 spectral = csvread(path_spectral,2,0);
+
+% band pass filter efficiency curves
+BPFilter700 = csvread("calibration_data/eff700",2,0);
+BPFilter950 = csvread("calibration_data/eff950",2,0);
