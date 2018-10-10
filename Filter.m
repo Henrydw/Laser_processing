29f1d23718214f75528b4a52a1a9484e3c6f7d4c
Harry de Winton <harrydewinton@gmail.com>
2018-10-10 13:31:38 +0100

    added laoding of camera spectral data

--- Filter.m
+++ Filter.m
@@ -1,6 +1,11 @@
 
 
-%% import ren_cal(.csv) data:
+%% imports:
+
+% ren_cal(.csv) data:
 path_ren_cal = "calibration_data/ren_cal.csv";
 optical_data = csvread(path_ren_cal,2,0);
 
+% load spectral repsonse of camera:
+path_spectral = "calibration_data/SA5_Spectral_Response_Curve.csv";
+spectral = csvread(path_spectral,2,0);
