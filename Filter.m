030ec624434f164d15fc5d99f125be4a9ebd430c
Harry de Winton <harrydewinton@gmail.com>
2018-10-11 10:07:03 +0100

    added readcih.m function and final parts of filter.m

--- Filter.m
+++ Filter.m
@@ -15,9 +15,9 @@ ORIGINAL_PIXEL_SIZE = 26.3e-6; % this is actually the source pixel size (not the
 path_ren_cal = "calibration_data/ren_cal.csv";
 optical_data = csvread(path_ren_cal,2,0);
 
-% spectral repsonse of camera:
-path_spectral = "calibration_data/SA5_Spectral_Response_Curve.csv";
-spectral = csvread(path_spectral,2,0);
+% spectral_response repsonse of spectral_response:
+path_spectral_response = "calibration_data/SA5_Spectral_Response_Curve.csv";
+spectral_response = csvread(path_spectral_response,2,0);
 
 % band pass filter efficiency curves
 BPFilter700 = csvread("calibration_data/eff700.csv",2,0);
@@ -44,9 +44,38 @@ I = bb_spectrum(WL,T);
 % Calculate amount of light collected via lens
 % Solid_angle = Area_of_lens/(dist_to_object^2)
 SA = pi()*10^2/420^2; % Focal length is 420mm, limiting appature is r=10mm
-Asource = (ORIGINAL_PIXEL_SIZE)^2; % Area of source (20um pixel size with 1:1 mag with high speed camera)
+Asource = (ORIGINAL_PIXEL_SIZE)^2; % Area of source (20um pixel size with 1:1 mag with high speed spectral_response)
 TP = Asource*SA; % Throughput in m^2.strad
 
+I = I * TP; % Watts/m - energy emitted/second per m wave length
 
+% Load optics transmission effeciency vs wavelength data
+optical_data(:,2) = optical_data(:,2)/max(optical_data(:,2)); % normalize data
+optics_eff = interp1(optical_data(:,1),optical_data(:,2),WL); % get efficiency at desired wavelengths
+ 
+I = I .* optics_eff; % correct for optics eff
 
+% apply colour filter
+filter_eff=interp1(bp_filter(:,1),bp_filter(:,2),WL);
+I=I.*filter_eff;
 
+% camera responsivity data vs wave length
+camera_resp = interp1(spectral_response(:,1),spectral_response(:,2),WL); %Amps/Watt
+
+I = I .* camera_resp; % Camera signal in amps/m wavelength
+
+Itotal = trapz(wl * 10^-9,I,2); % Integrate over wl to get total sensor ampage
+
+%% NOT WORKING - NEED TO GET ColorBit DATA
+
+% 1 amps(A) is 1 coulomb/s  which is 6.241*10^18 e-/s as a function of elementary charge e-
+% Signal output is amps*6.241e18*exposuretime/(well_capcity)*bitdepth
+coloumb = 6.241e18;
+ex_time = 10e-5; % Exposure time (Can vary to change the temperature range)
+wc = 45000; % Well capacity of sensor, units e-
+bd = 2^(imagedata.ColorBit) - 1; % 12 bit sensor 
+            
+% Calculate intensity value 
+% Note that if temperature range is to be determined, need to
+% multiply by an emissivity value ***
+Itotal = (Itotal * coloumb * ex_time / wc) * bd;
---
+++ readcih.m
@@ -0,0 +1,83 @@
+function imagedata = readcih(folderName)
+% readcih.m
+% READCIH Read Photron image data into MATLAB from .cih file
+% -------------------------------------------------------------
+% Remarks
+% This function is to be executed before readmraw function.
+% This function has been modified from the original readmraw function to 
+% serve the purpose of FYP
+% -------------------------------------------------------------
+% 
+% -------------------------------------------------------------
+% Examples
+% Load image data
+% imageData = readcih(fileID_cih);
+% -------------------------------------------------------------
+% 
+% Author: Adrian Kai Chwen Eu
+% Project: Thermal Imaging of metal 3D printing process
+% Date: 12/11/16
+
+fileName=[folderName filesep 'C001H001S0001.cih'];
+fileID = fopen(fileName);
+
+if fileID < 1
+    error('.cih file not found - %s', fileName);
+end
+
+% Read necessary information from .cih file
+    % Just need to read in information from 1 .cih file since both cih
+    % files are the same
+    % Algorithm to find the header title and obtain the corresponding value
+    % Critical information including
+    % a) Total Frame
+    % b) Image Width
+    % c) Image Height
+    % d) FPS
+    % e) Color Bit
+    headerCount = 0;
+    Header=textscan(fileID,'%s','delimiter',':');
+    Length = length(Header{1});
+    for i = 1:Length
+        if ischar(Header{1}{i})
+            if strcmpi('Total Frame ',Header{1}{i})
+                Total_Frames = str2double(cell2mat(Header{1}(i+1)));
+                headerCount = headerCount + 1;
+            end
+            if strcmpi('Image Width ',Header{1}{i})
+                Width = str2double(cell2mat(Header{1}(i+1)));
+                headerCount = headerCount + 1;
+            end
+            if strcmpi('Image Height ',Header{1}{i})
+                Height = str2double(cell2mat(Header{1}(i+1)));
+                headerCount = headerCount + 1;
+            end
+            if strcmpi('Record rate(fps) ',Header{1}{i})
+                FrameRate = str2double(cell2mat(Header{1}(i+1)));
+                headerCount = headerCount + 1;
+            end
+            if strcmpi('Color Bit ',Header{1}{i})
+                ColorBit = str2double(cell2mat(Header{1}(i+1)));
+                headerCount = headerCount + 1;
+            end
+        end
+        % If header count reaches 5, break the loop
+        if headerCount == 5
+            break;
+        end
+    end
+
+
+fclose(fileID);
+
+
+% Assign to corresponding image data
+imagedata.Header = Header;
+imagedata.Width = Width;
+imagedata.Height = Height;
+imagedata.Pixels = Width * Height;
+imagedata.ColorBit = ColorBit;
+imagedata.TotalFrames = Total_Frames;
+imagedata.FrameRate = FrameRate;
+
+end
