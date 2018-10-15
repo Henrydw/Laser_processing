f8b394b834284d81b6fa5dee4f32da26d3ea71af
Harry de Winton <harrydewinton@gmail.com>
2018-10-15 16:16:06 +0100

    make sure t_frame is == in length to TotalFrames

--- generate_image_alignment_surfaces.m
+++ generate_image_alignment_surfaces.m
@@ -57,7 +57,7 @@ IC2=readmraw(imagedata.folderName,imagedata,frange,2);
 % Calculate time vector for images up to speficied end frame
 start_frame = 1;
 vdt = 1/imagedata.FrameRate;
-t_frame = (0:(end_frame-1)) * vdt; %video frame time
+t_frame = (0:(end_frame)) * vdt; %video frame time
 t_frame = round(t_frame,9); %round to ns to avoid rounding errors
 
 % Filter for peak (spot finding) function
@@ -118,26 +118,28 @@ end
 % Read laser data file
 [t_daq,Diode,~,~,x,y,~] = importfile("example_data/100W_400us_100000fps.mat");
 % % ipm
-% % % Create a kaiser filter to smooth DAQ data
-% % 
-% % % Hard coded settings
-% % F_PASS = 0; % Pass frequency
-% % F_STOP = 2500; % Stop frequency
-% % RIPPLE = 0.0001; % Max bandpass ripple
-% % % Create filter
-% % dt = t_daq(3) - t_daq(2); % DAQ sample interval
-% % Fs = 1/dt; % Sampling freq
-% % [n, w, beta, ftype] = kaiserord([F_PASS,F_STOP], [1,0], [RIPPLE,RIPPLE], Fs);
-% % b = fir1(n,w,ftype,kaiser(n+1,beta),'noscale');
-% % 
-% % % Filter the data and then get xy mirror pos values at video frame times
-% % y_pos = interp1(t_daq,filtfilt(b,1,y(1:end)),t_frame);
-% % x_pos = interp1(t_daq,filtfilt(b,1,x(1:end)),t_frame);
-% % % Load in laser pulse data and get value at video frame times
-% % lpulse = interp1(t_daq,Diode(1:end),t_frame);
-% % 
-% % 
-% % 
-% % 
+% Create a kaiser filter to smooth DAQ data
 
+% Hard coded settings
+F_PASS = 0; % Pass frequency
+F_STOP = 2500; % Stop frequency
+RIPPLE = 0.0001; % Max bandpass ripple
+% Create filter
+dt = t_daq(3) - t_daq(2); % DAQ sample interval
+Fs = 1/dt; % Sampling freq
+[n, w, beta, ftype] = kaiserord([F_PASS,F_STOP], [1,0], [RIPPLE,RIPPLE], Fs);
+b = fir1(n,w,ftype,kaiser(n+1,beta),'noscale');
+
+% Filter the data and then get xy mirror pos values at video frame times
+y_pos = interp1(t_daq,filtfilt(b,1,y(1:end)),t_frame);
+x_pos = interp1(t_daq,filtfilt(b,1,x(1:end)),t_frame);
+% Load in laser pulse data and get value at video frame times
+lpulse = interp1(t_daq,Diode(1:end),t_frame);
+
+% Find indexes when laser is on (to reduce noise from times when it is off)
+lon = lpulse > 0.1; %0.1 seems a good value for 200W pulses
+%only use row where a spot was found and laser on (fixes warnings about NaN
+%in fit data)
+idxfit1=isfinite(IC1_p(1,:))&lon;
+idxfit2=isfinite(IC2_p(1,:))&lon;
 
