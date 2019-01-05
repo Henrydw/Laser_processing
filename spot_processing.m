d6e199479c428b0ad9f8a95f022acc565f0e96ea
Henry de Winton <harrydewinton@gmail.com>
2019-01-05 19:39:24 +0000

    spot_processing fixed and new Otsu section started

--- spot_processing.m
+++ spot_processing.m
@@ -44,10 +44,6 @@
 
 function [results]=spot_processing(folder_path,folder_name,intensity_ratio)
     %% Hard Coded stuff
-
-    DEFAULT_FILTER_THRESHOLD = 50;
-    REF_TEMPERATURE = (293:0.5:5000)';
-
     MELT_THRESHOLD = 1370;
 
     % from IMAGE DATA class
@@ -89,12 +85,16 @@ function [results]=spot_processing(folder_path,folder_name,intensity_ratio)
     % offset to rest of images assuming it is the same.
 
     
-    resamplelevel = 5;
+    resamplelevel = 2;
     
-    results.pyro = zeros(128*resamplelevel,128*resamplelevel,imagedata.TotalFrames);
+    framesize = imagedata.Width*resamplelevel; %assume square frame
+    
+    results.pyro = zeros(framesize,framesize,imagedata.TotalFrames);
 
     results.IC1s = results.pyro;
     results.IC2s = results.pyro;
+    results.Otsu_image = results.pyro;
+    results.spotradius = zeros(1,imagedata.TotalFrames);
 
     %midspottemp = zeros(1,end_frame);
     % = zeros(1,end_frame);
@@ -108,7 +108,7 @@ function [results]=spot_processing(folder_path,folder_name,intensity_ratio)
 
     
     % create meshgrid
-    [X,Y]=meshgrid(1:(imagedata.Width*resamplelevel),1:(imagedata.Height*resamplelevel));
+    [X,Y]=meshgrid(1:(framesize),1:(framesize));
 
     % load blocks loop
     for j=1:blockSize:end_frame
@@ -136,127 +136,94 @@ function [results]=spot_processing(folder_path,folder_name,intensity_ratio)
                 
 
                 %process camera 1
-                
                 %boost resolution
-                F = griddedInterpolant(double(IC1.raw(:,:,i)));
-                [sx,sy,~] = size(IC1.raw(:,:,i));
-                xq = (0:1/resamplelevel:sx)';
-                yq = (0:1/resamplelevel:sy)';
-                F.Method = 'linear';
-                resample = uint16(F({xq,yq}));
-                IC1.resample(:,:,frame_number) = resample(1:imagedata.Height*resamplelevel,1:imagedata.Height*resamplelevel,frame_number);
-                
-                % find peak, compensate and store
-                intensities = [];
-                
-                p1=FastPeakFind(IC1.resample(:,:,frame_number),DEFAULT_FILTER_THRESHOLD,filt,3,2);
-                for q = 1:2:size(p1)
-                    intensities = [intensities,IC1.resample(round(p1(q+1)),round(p1(q)),frame_number)];
-                end
-                true_peak = find(intensities==max(intensities));
-                p1=p1(odds(true_peak):odds(true_peak)+1);
-                IC1.x_store = [IC1.x_store,p1(1)];
-                IC1.y_store = [IC1.y_store,p1(2)];
-                X1=X-p1(1)+(imagedata.Width*resamplelevel)/2;
-                Y1=Y-p1(2)+(imagedata.Height*resamplelevel)/2;
+                IC1.resample(:,:,frame_number)=ResBoost(IC1.raw(:,:,i),resamplelevel);
+                % find peak, translate image and store offset
+                [x1_pos,y1_pos,IC1.x_store,IC1.y_store] = PeakFinder(IC1.resample(:,:,frame_number),IC1.x_store,IC1.y_store);
+                X1=X-x1_pos+framesize/2;
+                Y1=Y-y1_pos+framesize/2;
                 results.IC1s(:,:,frame_number)=interp2(X1,Y1,double(IC1.resample(:,:,i)),X,Y);
 
                 
                 %process camera 2
-                
                 %boost resolution
-                
-                
-                intensities = [];                
-                p2=FastPeakFind(IC2.raw(:,:,frame_number),DEFAULT_FILTER_THRESHOLD,filt,3,2);
-                for q = 1:2:size(p2)
-                    intensities = [intensities,IC2.raw(round(p2(q+1)),round(p2(q)),frame_number)];
-                end
-                true_peak = find(intensities==max(intensities));
-                p2=p2(odds(true_peak):odds(true_peak)+1);
-                IC2.x_store = [IC2.x_store,p2(1)];
-                IC2.y_store = [IC2.y_store,p2(2)];
-                X2=X-p2(1)+imagedata.Width/2;
-                Y2=Y-p2(2)+imagedata.Height/2;
-                IC2_new=interp2(X2,Y2,double(IC2.raw(:,:,i)),X,Y);
-                results.IC2s(:,:,frame_number) = IC2_new;
-                
+                IC2.resample(:,:,frame_number)=ResBoost(IC2.raw(:,:,i),resamplelevel);
+                % find peak, translate image and store offset
+                [x2_pos,y2_pos,IC2.x_store,IC2.y_store] = PeakFinder(IC2.resample(:,:,frame_number),IC2.x_store,IC2.y_store);
+                X2=X-x2_pos+framesize/2;
+                Y2=Y-y2_pos+framesize/2;
+                results.IC2s(:,:,frame_number)=interp2(X2,Y2,double(IC2.resample(:,:,i)),X,Y);
                 
                 % temperature comparison
-                imratio = IC1_new./IC2_new;
-                imratio(isnan(imratio)) = 0;
-                imratio(isinf(imratio)) = 0;
-                immultiply = IC1_new.* IC2_new;
-                imratio(immultiply < DEFAULT_FILTER_THRESHOLD) = 0;
+                results.pyro(:,:,frame_number) = Pyro(results.IC1s(:,:,frame_number),results.IC2s(:,:,frame_number),intensity_ratio);
                 
-                image_temp = interp1(intensity_ratio,REF_TEMPERATURE,imratio);
-                image_temp(isnan(image_temp)) = 0;
+                %analysis:
+                results.spotradius(frame_number) = findSpotRadius(results.pyro(:,:,frame_number), MELT_THRESHOLD);
                 
-                results.pyro(:,:,frame_number) = image_temp;
+                bw = imbinarize(results.pyro(:,:,frame_number));
+                bw = bwareaopen(bw,5);
+                cc = bwconncomp(bw,4);
+                frame_number = frame_number+1;
+                graindata = regionprops(cc,'basic');
+                grain_areas = [graindata.Area];
+                [max_area, idx] = max(grain_areas);
+                grain = false(size(bw));
+                grain(cc.PixelIdxList{idx}) = true;
+                Otsu_image = results.pyro(:,:,frame_number);
+                Otsu_image(grain==0)=0;
+                results.Otsu_image(:,:,frame_number)=Otsu_image;
                 
                 frame_number = frame_number+1;               
             end
             
             IC1.x_mean = mean(IC1.x_store);
-            X1=X-IC1.x_mean+imagedata.Width/2;
+            X1=X-IC1.x_mean+framesize/2;
             IC1.y_mean = mean(IC1.y_store);
-            Y1=Y-IC1.y_mean+imagedata.Width/2;
+            Y1=Y-IC1.y_mean+framesize/2;
             IC2.x_mean = mean(IC2.x_store);
-            X2=X-IC2.x_mean+imagedata.Width/2;
+            X2=X-IC2.x_mean+framesize/2;
             IC2.y_mean = mean(IC2.y_store);
-            Y2=Y-IC2.y_mean+imagedata.Width/2;
+            Y2=Y-IC2.y_mean+framesize/2;
             
         else
             % process frames loop
             for i=1:blockSize
                 
-                
                 % camera 1
-                IC1_new=interp2(X1,Y1,double(IC1.raw(:,:,i)),X,Y);
-                results.IC1s(:,:,frame_number) = IC1_new;
+                
+                %boost resolution
+                IC1.resample(:,:,frame_number)=ResBoost(IC1.raw(:,:,i),resamplelevel);
+                results.IC1s(:,:,frame_number)=interp2(X1,Y1,double(IC1.resample(:,:,frame_number)),X,Y);
 
                 % camera 2
-                IC2_new=interp2(X2,Y2,double(IC2.raw(:,:,i)),X,Y);
-                results.IC2s(:,:,frame_number) = IC2_new;
                 
+                %boost resolution
+                IC2.resample(:,:,frame_number)=ResBoost(IC2.raw(:,:,i),resamplelevel);
+                results.IC2s(:,:,frame_number)=interp2(X2,Y2,double(IC2.resample(:,:,frame_number)),X,Y);
                 
                 % temperature comparison
-                imratio = IC1_new./IC2_new;
-                imratio(isnan(imratio)) = 0;
-                imratio(isinf(imratio)) = 0;
-                immultiply = IC1_new.* IC2_new;
-                imratio(immultiply < DEFAULT_FILTER_THRESHOLD) = 0;
+                results.pyro(:,:,frame_number) = Pyro(results.IC1s(:,:,frame_number),results.IC2s(:,:,frame_number),intensity_ratio);
                 
-                image_temp = interp1(intensity_ratio,REF_TEMPERATURE,imratio);
-                image_temp(isnan(image_temp)) = 0;
+                %analysis:
+                results.spotradius(frame_number) = findSpotRadius(results.pyro(:,:,frame_number), MELT_THRESHOLD);
                 
-                results.pyro(:,:,frame_number) = image_temp;
+                bw = imbinarize(results.pyro(:,:,frame_number));
+                bw = bwareaopen(bw,5);
+                cc = bwconncomp(bw,4);
+                graindata = regionprops(cc,'basic');
+                grain_areas = [graindata.Area];
+                [max_area, idx] = max(grain_areas);
+                grain = false(size(bw));
+                grain(cc.PixelIdxList{idx}) = true;
+                Otsu_image = results.pyro(:,:,frame_number);
+                Otsu_image(grain==0)=0;
+                results.Otsu_image(:,:,frame_number)=Otsu_image;
                 
-                frame_number = frame_number+1;   
-                
+                frame_number = frame_number+1;
+
             end
         end
-            
 
-%         %%%%% RATIO TO TEMP NEEDS TO BE RE_MADE %%%%%%%%%        %calculate temp image from two images
-%             % Find ratio between camera images intensities
-%         imratio = IC1f./IC2f;
-%         imratio(isnan(imratio)) = 0;
-%         imratio(isinf(imratio)) = 0;
-% 
-%         image_temp = interp1(intensity_ratio,REF_TEMPERATURE,imratio);
-%         image_temp(isnan(image_temp)) = 293;
-% 
-%         % Multiply two camera image intensities together and compare against a
-%         % threshold value. If lower, it means the pixel is noisy and will be
-%         % registered as 293K
-%         immultiply = IC1f .* IC2f;
-%         %image_temp(immultiply < DEFAULT_FILTER_THRESHOLD) = NaN;
-% 
-%         % save image to file (append?? - cannot be stored in memory)
-%         % for now simply append - to 3D array (small enough for sample data
-%         results.processed_images(:,:,frame_number) = image_temp;
-% 
 %         %midspottemp(frame_number) = findMidSpotTemp(image_temp);
 %         %meanmidspottemp(frame_number) = findMeanMidSpotTemp(image_temp);
 %         %peakspottemp(frame_number) = findPeakTemp(image_temp);
@@ -267,7 +234,62 @@ function [results]=spot_processing(folder_path,folder_name,intensity_ratio)
     end
 end
 
+function []=frame_analysis(thermal_image)
+    
+end
 
+function [x_pos,y_pos,x_store,y_store] = PeakFinder(img,x_store,y_store)
+    % find peaks in image using Fast Peak find and returns the x and y
+    % position of the highest one. Append to x and y lists is they are
+    % entered.
+    FILTER_THRESHOLD = 300;
+    filt=(fspecial('gaussian', 7,1));
+    odds = [1:2:1000];
+    intensities = [];
+
+    peaks=FastPeakFind(img,FILTER_THRESHOLD,filt,3,2);
+    %assert(length(peaks)>=2,'No peaks found')
+    for q = 1:2:size(peaks)
+        intensities = [intensities,img(round(peaks(q+1)),round(peaks(q)))];
+    end
+    true_peak = find(intensities==max(intensities));
+    x_pos = peaks(odds(true_peak));
+    y_pos = peaks(odds(true_peak)+1);
+    
+    if(nargin>1)
+        x_store = [x_store,x_pos];
+        y_store = [y_store,y_pos];
+    end
+end
+
+function thermal_image = Pyro(img1,img2,intensity_ratio)
+    % temperature from ratio and look up table(intensity_ratio and REF
+    % TEMPERATURE)
+    
+    FILTER_THRESHOLD = 300;
+    REF_TEMPERATURE = (293:0.5:5000)';
+    
+    imratio = img1./img2;
+    imratio(isnan(imratio)) = 0;
+    imratio(isinf(imratio)) = 0;
+    immultiply = img1.* img2;
+    imratio(immultiply < FILTER_THRESHOLD) = 0;
+
+    thermal_image = interp1(intensity_ratio,REF_TEMPERATURE,imratio);
+    thermal_image(isnan(thermal_image)) = 0;
+end
+
+function resample = ResBoost(raw_image,resamplelevel)
+    assert(isa(raw_image,'uint16'),'raw image is not type: uint16')
+    F = griddedInterpolant(double(raw_image));
+    [sx,sy,~] = size(raw_image);
+    xq = (0:1/resamplelevel:sx)';
+    yq = (0:1/resamplelevel:sy)';
+    F.Method = 'linear';
+    resample = uint16(F({xq,yq}));
+    height = sx*resamplelevel;
+    resample = resample(1:height,1:height);
+end
 
 function midtemp = findMidSpotTemp(imgtemp)
     %midtemp = imgtemp(size(imgtemp,1)/2,size(imgtemp,2)/2);
